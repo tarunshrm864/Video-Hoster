e45a60e9b9266f3c2f2177f8d344c27c64bac555
Tarun <tarunshrm864@gmail.com>
2020-05-05 23:35:01 +0530

    final commit

---
+++ videohoster-service/src/main/java/com/upgrad/videohoster/service/business/AdminService.java
@@ -0,0 +1,65 @@
+package com.upgrad.videohoster.service.business;
+
+
+import com.upgrad.videohoster.service.dao.VideoDao;
+import com.upgrad.videohoster.service.entity.VideoEntity;
+import com.upgrad.videohoster.service.entity.UserAuthTokenEntity;
+import com.upgrad.videohoster.service.exception.VideoNotFoundException;
+import com.upgrad.videohoster.service.exception.UnauthorizedException;
+import com.upgrad.videohoster.service.exception.UserNotSignedInException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private VideoDao videoDao;
+
+    public VideoEntity getVideo(final String videoUuid, final String authorization) throws VideoNotFoundException, UnauthorizedException, UserNotSignedInException {
+
+        UserAuthTokenEntity userAuthTokenEntity = videoDao.getUserAuthToken(authorization);
+        VideoEntity videoEntity = videoDao.getVideo(videoUuid);
+        if (videoEntity == null) {
+            throw new VideoNotFoundException("VID-001", "Video not found");
+        }
+        String role = userAuthTokenEntity.getUser().getRole();
+        if (role.equals("admin")) {
+//            VideoEntity videoEntity = videoDao.getVideo(videoUuid);
+            if (videoEntity == null) {
+                throw new VideoNotFoundException("VID-001", "Video found");
+            }
+            return videoEntity;
+        } else
+            throw new UnauthorizedException("ATH-001", "Unauthorized Access");
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public VideoEntity updateVideo(final VideoEntity videoEntity, final String authorization) throws VideoNotFoundException, UnauthorizedException, UserNotSignedInException {
+        UserAuthTokenEntity userAuthTokenEntity = videoDao.getUserAuthToken(authorization);
+        if (userAuthTokenEntity == null) {
+            throw new UserNotSignedInException("USR-001", "You are not Signed in, sign in first to update the details of the video");
+        }
+
+        String role = userAuthTokenEntity.getUser().getRole();
+        if (role.equals("admin")) {
+
+            VideoEntity existingvideoEntity = videoDao.getVideoById(videoEntity.getId());
+
+            if (existingvideoEntity == null) {
+                throw new VideoNotFoundException("VID-002", "Video with given id is not found");
+            }
+            videoEntity.setUuid(existingvideoEntity.getUuid());
+            videoEntity.setNo_of_likes(existingvideoEntity.getNo_of_likes());
+            videoEntity.setUser_id(existingvideoEntity.getUser_id());
+            videoEntity.setCreated_at(existingvideoEntity.getCreated_at());
+
+            return videoDao.updateVideo(videoEntity);
+
+        } else {
+            throw new UnauthorizedException("ATH-001","Unauthorized Access");
+        }
+    }
+}
---
+++ videohoster-service/src/main/java/com/upgrad/videohoster/service/dao/VideoDao.java
@@ -0,0 +1,53 @@
+package com.upgrad.videohoster.service.dao;
+
+import com.upgrad.videohoster.service.entity.VideoEntity;
+import com.upgrad.videohoster.service.entity.UserAuthTokenEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class VideoDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public VideoEntity createVideo(VideoEntity videoEntity) {
+
+        entityManager.persist(videoEntity);
+        return videoEntity;
+    }
+
+    public UserAuthTokenEntity getUserAuthToken(final String accesstoken) {
+        try {
+            return entityManager.createNamedQuery("userAuthTokenByAccessToken", UserAuthTokenEntity.class).setParameter("accessToken", accesstoken).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    public VideoEntity getVideo(final String videoUuid) {
+        try {
+            return entityManager.createNamedQuery("VideoEntityByUuid", VideoEntity.class).setParameter("uuid", videoUuid).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    public VideoEntity getVideoById(final long Id) {
+        try
+        {
+            return entityManager.createNamedQuery("VideoEntityById",VideoEntity.class).setParameter("id",Id).getSingleResult();
+        }
+        catch (NoResultException nre)
+        {
+            return null;
+        }
+    }
+
+    public VideoEntity updateVideo(final VideoEntity videoEntity) {
+        return entityManager.merge(videoEntity);
+    }
+}
